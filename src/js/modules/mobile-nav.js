fbc694ee923f80ca422016c5e70af932b7540bc9
Kyrylo <computer45456@gmail.com>
2024-06-12 10:33:57 +0300

    start on notebook

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,43 @@
 function mobileNav() {
 	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
+	// const navBtn = document.querySelector('.mobile-nav-btn');
+	const navBtn = document.querySelector('.hamburger');
 	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+	// const menuIcon = document.querySelector('.nav-icon');
+	// const menuIcon = document.querySelector('.line');
 
 	navBtn.onclick = function () {
 		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
+		// menuIcon.classList.toggle('nav-icon--active');
 		document.body.classList.toggle('no-scroll');
 	};
 }
 
+
+// function mobileNav() {
+// 	// Mobile nav button
+// 	const navBtn = document.querySelector('.mobile-nav');
+// 	const nav = document.querySelector('.mobile-nav-btn-box');
+// 	const menuIcon = document.querySelector('.nav-icon');
+
+// 	navBtn.onclick = function () {
+// 		nav.classList.toggle('mobile-nav--open');
+// 		menuIcon.classList.toggle('nav-icon--active');
+// 		document.body.classList.toggle('no-scroll');
+// 	};
+// }
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	const hamburgers = document.querySelectorAll('.hamburger');
+
+	hamburgers.forEach(function (hamburger) {
+		hamburger.addEventListener('click', function () {
+			this.classList.toggle('is-active');
+		});
+	});
+});
 export default mobileNav;
